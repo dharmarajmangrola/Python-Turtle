d64c74cc78efe2f8a15032c6638a2b58ccfc19db
dharmarajmangrola <84985746+dharmarajmangrola@users.noreply.github.com>
2022-08-05 09:54:33 +0530

    Add files via upload

---
+++ Graphics_Window.py
@@ -0,0 +1,17 @@
+#import turtle library
+#github:-dharmarajmangrola
+
+import turtle
+
+dharmaraj_window = turtle.Screen()
+dharmaraj_window.bgcolor("blue")
+ 
+# create graphic window
+
+dharmaraj_pen = turtle.Turtle()
+dharmaraj_pen.forward(150)  
+dharmaraj_pen.left(90)               
+dharmaraj_pen.forward(75)
+dharmaraj_pen.color("white")
+dharmaraj_pen.pensize(12)
+
